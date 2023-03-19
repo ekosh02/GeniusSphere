d344ce955aa254bcddc752db3fe00ef274131bd3
ekosh02 <turapbaeveldos@gmail.com>
2023-03-19 19:03:44 +0600

    for object

--- src/utils/AsyncStorage.js
+++ src/utils/AsyncStorage.js
@@ -9,10 +9,31 @@ export const setStorage = async (key, value) => {
   }
 };
 
+export const setStorageObject = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
 export const getStorage = async key => {
   try {
     const responce = await AsyncStorage.getItem(key);
     return responce;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+export const getStorageObject = async key => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    console.log('jsonValue', jsonValue);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
   } catch (error) {
     console.log(error);
     return;
